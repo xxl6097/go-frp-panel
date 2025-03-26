6bbda39505fff8ce5439abc9cfafb640e6a614b5
M4MacBookAir <M4MacBookAir@clife.cn>
2025-03-26 18:39:41 +0800

    v0.0.6 by uuxia

--- build.sh
+++ build.sh
@@ -356,26 +356,75 @@ function buildAllUploadGithub() {
   github_release
 }
 
+function showBuildDir() {
+  # 检查是否输入路径参数
+  if [ -z "$1" ]; then
+      echo "用法: $0 <路径>"
+      exit 1
+  fi
+
+  # 验证路径是否存在且为目录
+  if [ ! -d "$1" ]; then
+      echo "错误: 路径 '$1' 不存在或不是目录！"
+      exit 1
+  fi
+
+  # 获取指定路径下的所有直接子目录（非递归）
+  dirs=()
+  while IFS= read -r dir; do
+      dirs+=("$dir")
+  done < <(find "$1" -maxdepth 1 -type d ! -path "$1" | sort)
+
+  # 检查是否有子目录
+  if [ ${#dirs[@]} -eq 0 ]; then
+      echo "路径 '$1' 下没有子目录！"
+      exit 0
+  fi
+
+  # 生成交互式菜单
+  echo "请选择要操作的目录："
+  PS3="输入序号 (1-${#dirs[@]}): "
+  select dir in "${dirs[@]}"; do
+      if [[ -n "$dir" ]] && [[ $REPLY -ge 1 && $REPLY -le ${#dirs[@]} ]]; then
+          echo "您选择的目录是: $dir"
+          break
+#          return $dir
+      else
+          echo "无效输入！请输入有效序号。"
+      fi
+  done
+}
+# shellcheck disable=SC2120
+function buildDir() {
+  showBuildDir ./cmd
+  builddir="./dist/${dir}"
+  appname=$(basename "$dir")
+  appdir=${dir}
+  disname="${dir}应用程序"
+  describe="一款基于GO语言的${dir}程序"
+  rm -rf ${builddir}
+  buildMenu $builddir $appname "$version" $appdir $disname $describe
+}
+
 function main() {
+  initCommArgs
   echo "1、编译Frps"
   echo "2、编译Frpc"
   echo "3、编译全部"
-  echo "4、上传github"
+  echo "4、编译目录"
   read -p "请选择：" index
   if [ $index == 1 ]; then
-    initCommArgs
-    gitCommit
     buildFrpsMenu
   elif [ $index == 2 ]; then
-    initCommArgs
-    gitCommit
     buildFrpcMenu
   elif [ $index == 3 ]; then
-    initCommArgs
-    gitCommit
     buildFrpcAndFrpsAll
   elif [ $index == 4 ]; then
-    buildAllUploadGithub
+    buildDir
+  fi
+  #提交代码
+  if [ $index -le 3 ]; then
+      gitCommit
   fi
 }
 
--- test.sh
+++ test.sh
@@ -1,59 +1,49 @@
 #!/bin/bash
 
-options=("darwin:amd64" "darwin:arm64" "freebsd:amd64" "linux:amd64" "linux:arm:7" "linux:arm:5" "linux:arm64" "windows:amd64" "windows:arm64" "linux:mips64" "linux:mips64le" "linux:mips:softfloat" "linux:mipsle:softfloat" "linux:riscv64" "linux:loong64" "android:arm64")
+function showBuildDir() {
+  # 检查是否输入路径参数
+  if [ -z "$1" ]; then
+      echo "用法: $0 <路径>"
+      exit 1
+  fi
 
-function buildMenu() {
-    PS3="请选择需要编译的平台："
-    select arch in "${options[@]}"; do
-          if [[ -n "$arch" ]]; then
-            IFS=":" read -r os arch extra <<< "$arch"
-                  #echo "OS: $os | Arch: $arch | extra: ${extra}"
-                  dstFilePath=./dist/${appname}_${version}_${os}_${arch}
-                  flags='';
-                  if [ "${os}" = "linux" ] && [ "${arch}" = "arm" ] && [ "${extra}" != "" ] ; then
-                    if [ "${extra}" = "7" ]; then
-                      flags=GOARM=7;
-                      dstFilePath=./dist/${appname}_${version}_${os}_${arch}hf
-                    elif [ "${extra}" = "5" ]; then
-                      flags=GOARM=5;
-                      dstFilePath=./dist/${appname}_${version}_${os}_${arch}
-                    fi;
-                  elif [ "${os}" = "windows" ] ; then
-                    dstFilePath=./dist/${appname}_${version}_${os}_${arch}.exe
-                  elif [ "${os}" = "linux" ] && ([ "${arch}" = "mips" ] || [ "${arch}" = "mipsle" ]) && [ "${extra}" != "" ] ; then
-                    flags=GOMIPS=${extra};
-                  fi;
-                  echo "build：GOOS=${os} GOARCH=${arch} ${flags} ==>${dstFilePath}"
-                  env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} ${flags} go build -trimpath -ldflags "$ldflags -s -w -linkmode internal" -o ${dstFilePath} ${appdir}
-          else
-            cho "输入无效，请重新选择。"
-          fi
-    done
-}
+  # 验证路径是否存在且为目录
+  if [ ! -d "$1" ]; then
+      echo "错误: 路径 '$1' 不存在或不是目录！"
+      exit 1
+  fi
 
-function buildAll() {
-  for arch in "${options[@]}"; do
-      IFS=":" read -r os arch extra <<< "$arch"
-      #echo "OS: $os | Arch: $arch | extra: ${extra}"
-      dstFilePath=./dist/${appname}_${version}_${os}_${arch}
-      flags='';
-      if [ "${os}" = "linux" ] && [ "${arch}" = "arm" ] && [ "${extra}" != "" ] ; then
-        if [ "${extra}" = "7" ]; then
-          flags=GOARM=7;
-          dstFilePath=./dist/${appname}_${version}_${os}_${arch}hf
-        elif [ "${extra}" = "5" ]; then
-          flags=GOARM=5;
-          dstFilePath=./dist/${appname}_${version}_${os}_${arch}
-        fi;
-      elif [ "${os}" = "windows" ] ; then
-        dstFilePath=./dist/${appname}_${version}_${os}_${arch}.exe
-      elif [ "${os}" = "linux" ] && ([ "${arch}" = "mips" ] || [ "${arch}" = "mipsle" ]) && [ "${extra}" != "" ] ; then
-        flags=GOMIPS=${extra};
-      fi;
-      echo "build：GOOS=${os} GOARCH=${arch} ${flags} ==>${dstFilePath}"
-      env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} ${flags} go build -trimpath -ldflags "$ldflags -s -w -linkmode internal" -o ${dstFilePath} ${appdir}
+  # 获取指定路径下的所有直接子目录（非递归）
+  dirs=()
+  while IFS= read -r dir; do
+      dirs+=("$dir")
+  done < <(find "$1" -maxdepth 1 -type d ! -path "$1" | sort)
+
+  # 检查是否有子目录
+  if [ ${#dirs[@]} -eq 0 ]; then
+      echo "路径 '$1' 下没有子目录！"
+      exit 0
+  fi
+
+  # 生成交互式菜单
+  echo "请选择要操作的目录："
+  PS3="输入序号 (1-${#dirs[@]}): "
+  select dir in "${dirs[@]}"; do
+      if [[ -n "$dir" ]] && [[ $REPLY -ge 1 && $REPLY -le ${#dirs[@]} ]]; then
+          echo "您选择的目录是: $dir"
+          break
+#          return $dir
+      else
+          echo "无效输入！请输入有效序号。"
+      fi
   done
-  bash <(curl -s -S -L http://uuxia.cn:8087/up) ./dist /soft/${appname}/${version}
 }
 
-buildMenu
+function build() {
+    read -p "请选择：" index
+      if [ $index -le 3 ]; then
+        echo "无效输入！请输入有效序号。"
+      fi
+}
+
+build
