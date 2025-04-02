18ccffc4e23c3e413e97e066160e53f636773672
u-1 <u-1@usb.com>
2025-04-02 16:48:48 +0800

    v0.1.2 by

--- web/build.sh
+++ web/build.sh
@@ -23,7 +23,9 @@ function buildFrpcAndFrpsAll() {
 
   wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
   tar -xf node-v20.17.0-linux-x64.tar.xz
-  mv -r node-v20.17.0-linux-x64 node
+
+  rm -r ./node
+  mv -f node-v20.17.0-linux-x64 node
 
   rm -r /usr/local/bin/node
   rm -r /usr/local/bin/npm
