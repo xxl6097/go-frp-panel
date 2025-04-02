b90d627b0e1b53e37ea601cacc0e8095a9d4adf9
u-1 <u-1@usb.com>
2025-04-02 16:56:50 +0800

    v0.1.5 by

--- web/build.sh
+++ web/build.sh
@@ -21,17 +21,18 @@ function buildFrpcAndFrpsAll() {
 #  wget https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-x64.tar.xz
 #  tar xvf node-v18.12.0-linux-x64.tar.xz
 
-  wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
-  tar xvf node-v20.17.0-linux-x64.tar.xz
+  wget https://nodejs.org/dist/v20.19.0/node-v20.19.0-linux-x64.tar.xz
+  tar xvf node-v20.19.0-linux-x64.tar.xz
 
 
-  rm -r /usr/local/bin/node
-  rm -r /usr/local/bin/npm
+#  rm -r /usr/local/bin/node
+#  rm -r /usr/local/bin/npm
   # 创建软链接到系统路径
-  sudo ln -s ./node-v20.17.0-linux-x64/bin/node /usr/local/bin/node
-  sudo ln -s ./node-v20.17.0-linux-x64/bin/npm /usr/local/bin/npm
-
+  sudo ln -s ./node-v20.19.0-linux-x64/bin/node /usr/local/bin/node
+  sudo ln -s ./node-v20.19.0-linux-x64/bin/npm /usr/local/bin/npm
+  echo "打印 node"
   node -v  # 应输出如v16.20.0
+  echo "打印 npm"
   npm -v   # 应输出如8.19.4
 
   echo "全局安装npm-run-all"
