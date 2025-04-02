6cd0947829cd38c8da1ae0eed788b88c16630fae
u-1 <u-1@usb.com>
2025-04-02 16:44:12 +0800

    v0.1.0 by

--- web/build.sh
+++ web/build.sh
@@ -18,12 +18,18 @@ function buildFrpcAndFrpsAll() {
 #  sudo apt update && sudo apt install make
 #  make --version
   # 下载并解压Node.js
-  wget https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-x64.tar.xz
-  tar xvf node-v18.12.0-linux-x64.tar.xz
+#  wget https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-x64.tar.xz
+#  tar xvf node-v18.12.0-linux-x64.tar.xz
 
+  wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
+  tar xvf node-v20.17.0-linux-x64.tar.xz
+  mv -r node-v20.17.0-linux-x64 node
+
+  rm -r /usr/local/bin/node
+  rm -r /usr/local/bin/npm
   # 创建软链接到系统路径
-  sudo ln -s ./node-v18.12.0-linux-x64/bin/node /usr/local/bin/node
-  sudo ln -s ./node-v18.12.0-linux-x64/bin/npm /usr/local/bin/npm
+  sudo ln -s ./node/bin/node /usr/local/bin/node
+  sudo ln -s ./node/bin/npm /usr/local/bin/npm
 
   node -v  # 应输出如v16.20.0
   npm -v   # 应输出如8.19.4
