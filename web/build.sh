293b5a7e8d0a0703b19423913b2351147c804090
u-1 <u-1@usb.com>
2025-04-02 16:25:40 +0800

    v0.0.94 by

--- web/build.sh
+++ web/build.sh
@@ -17,16 +17,22 @@ function buildFrps() {
 function buildFrpcAndFrpsAll() {
   sudo apt update && sudo apt install make
   make --version
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  source ~/.bashrc  # 或 source ~/.zshrc
-  #nvm install node
-  nvm install 18.11.12
-  node -v  # 输出 v16.20.0
-  npm -v   # 自动匹配对应版本（如 npm 8.19.4）
+  # 下载并解压Node.js
+  wget https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-x64.tar.xz
+  tar xvf node-v18.12.0-linux-x64.tar.xz
+
+  # 创建软链接到系统路径
+  sudo ln -s ./node-v18.12.0-linux-x64/bin/node /usr/local/bin/node
+  sudo ln -s ./node-v18.12.0-linux-x64/bin/npm /usr/local/bin/npm
+
+  node -v  # 应输出如v16.20.0
+  npm -v   # 应输出如8.19.4
+
   npm install npm-run-all --save-dev
   npx run-p --version  # 本地安装时使用
   run-p --version      # 全局安装时使用
   npm list npm-run-all  # 本地依赖
+  
   buildFrpc &
   buildFrps &
   wait  # 等待所有后台进程结束
