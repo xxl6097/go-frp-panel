d6c0d07a21d4153a7474d9f31d17f178a34e7253
xxl6097 <263996097@qq.com>
2026-07-18 15:41:41 +0800

    fix: 123

--- cmd/frpc/test/main.go
+++ cmd/frpc/test/main.go
@@ -60,7 +60,7 @@ func main() {
 		ClientCommonConfig: *ccc,
 		Proxies:            proxies,
 	}
-	z.Infof("ClientConfig:%+v", cfg)
+	z.Infof("ClientConfig: %+v", cfg)
 	frpc.SetCfgModel(&frpc.CfgModel{Frpc: *cfg})
 
 	err := frp.WriteFrpcMainConfigWithOut(cfg)
